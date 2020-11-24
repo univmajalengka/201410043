403703dc88fd1960f48e663b571d6f023036fc77
Elghiffari28 <elbag467@gmail.com>
2020-11-24 12:49:11 +0700

    Revisi

--- Tugas #4.cpp
+++ Tugas #4.cpp
@@ -6,32 +6,60 @@ int main()
 	cout<<"silahkan pakai dengan baik dan benar"<<endl;
 	cout<<"1. pertambahan \t\t\t 3. perkalian"<<endl;
 	cout<<"2. pengurangan \t\t\t 4. pembagian"<<endl;
+	cout<<"\t      5. Sisa Hasil Bagi"<<endl;
 	cout<<"_____________________________________________"<<endl;
 	int a,b,c,menu;
 	float d,e,f;
 	cin>>menu;
 	if(menu==1){
+		cout<<"Operasi Pertambahan"<<endl;
+		cout<<"Masukan Angka :"<<endl;
 		cin>>a;
+		cout<<"+"<<endl;
 		cin>>b;
 		c=a+b;
+		cout<<"Hasilnya Adalah :"<<endl;
 		cout<<c;
 	}else if(menu==2){
+		cout<<"Operasi Pengurangan"<<endl;
+		cout<<"Masukan Angka :"<<endl;
 		cin>>a;
+		cout<<"-"<<endl;
 		cin>>b;
 		c=a-b;
+		cout<<"Hasilnya Adalah :"<<endl;
 		cout<<c;
 	}else if(menu==3){
+		cout<<"Operasi Perkalian"<<endl;
+		cout<<"Masukan Angka :"<<endl;
 		cin>>a;
+		cout<<"X"<<endl;
 		cin>>b;
 		c=a*b;
+		cout<<"Hasilnya Adalah :"<<endl;
 		cout<<c;
 	}else if(menu==4){
+		cout<<"Operasi Pembagian"<<endl;
+		cout<<"Masukan Angka :"<<endl;
 		cin>>d;
+		cout<<"/"<<endl;
 		cin>>e;
 		f=d/e;
+		cout<<"Hasilnya Adalah :"<<endl;
 		cout<<f;
+	}else if(menu==5){
+		cout<<"Operasi Sisa Hasil Bagi"<<endl;
+		cout<<"Masukan Angka :"<<endl;
+		cin>>a;
+		cout<<"/"<<endl;
+		cin>>b;
+		c=a%b;
+		cout<<"Sisa Dari Pembagian Tersebut Adalah :"<<endl;
+		cout<<c;
 	}else{
-		cout<<"sayang sekali menu tidak ada :(";
+		
+		cout<<"sayang sekali menu tidak ada :("<<endl;
+		cout<<"Masukan Angka Yang Benar"<<endl;
 	}
 	
 	return false;
